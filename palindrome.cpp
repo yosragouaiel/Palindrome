75a3ead4c0067f4b71c9cad759634cdf301e5240
yosragouaiel <yosra.gouaiel@ensi-uma.tn>
2019-09-19 15:06:55 +0100

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,18 @@
 
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <strings.h>
+
+bool isPalindrome(const std::string& word)
+{
+   std::string rword(word.rbegin(),word.rend());
+    return (strcasecmp(word.c_str(),rword.c_str())==0);
+}
+
+#ifndef RunTests
+int main()
+{
+    std::cout << isPalindrome("Deleveled");
+}
+#endif
